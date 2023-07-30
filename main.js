660f10ac1f274ead85da7682c0f269b1e0131db5
ali <alidarwish502@gmail.com>
2023-07-30 16:48:42 +0200

    Prepearing for firebase.

--- main.js
+++ main.js
@@ -1,24 +1,8 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+const firebaseConfig = {
+  apiKey: "AIzaSyBFS6SC3f3MsQaY1ScYkafeVPiJm5Y4krQ",
+  authDomain: "firbase-9-ali.firebaseapp.com",
+  projectId: "firbase-9-ali",
+  storageBucket: "firbase-9-ali.appspot.com",
+  messagingSenderId: "361423392685",
+  appId: "1:361423392685:web:0d30ce4984d34269229b24"
+};
